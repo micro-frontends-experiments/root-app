29189200847c4ebb528af20f9e04fde97d609d60
danlaptev <danlaptev@yandex-team.ru>
2022-03-27 15:35:43 +0300

    MVP 1.0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import MicroAppLoader from './MicroAppLoader';
+
+const MICRO_APP_1_HOST = 'http://localhost:3000'
+const MICRO_APP_2_HOST = 'http://localhost:3001'
+
+const MicroApp1 = ({history}) => (
+    <MicroAppLoader history={history} host={MICRO_APP_1_HOST} name="MicroApp1"/>
+)
+
+const MicroApp2 = ({history}) => (
+    <MicroAppLoader history={history} host={MICRO_APP_2_HOST} name="MicroApp2"/>
+)
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    return (
+        <div className="App">
+            <h1>Root App</h1>
+            <div className="row">
+                <MicroApp1/>
+                <MicroApp2/>
+            </div>
+        </div>
   );
 }
 
---
+++ src/MicroAppLoader.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+class MicroAppLoader extends React.Component {
+    componentDidMount() {
+        const { name, host, document } = this.props;
+        const scriptId = `micro-frontend-script-${name}`;
+
+        if (document.getElementById(scriptId)) {
+            this.renderMicroApp();
+            return;
+        }
+
+        fetch(`${host}/asset-manifest.json`)
+            .then(res => res.json())
+            .then(manifest => {
+                console.log('manifest: ', manifest);
+                const script = document.createElement('script');
+                script.id = scriptId;
+                script.crossOrigin = '';
+                script.src = `${process.env.NODE_ENV === "production" ? host.slice(0, host.lastIndexOf('/')) : host}${manifest["files"]["main.js"]}`;
+                script.onload = this.renderMicroApp;
+                document.head.appendChild(script);
+            });
+    }
+
+    componentWillUnmount() {
+        const { name, window } = this.props;
+
+        window[`unmount${name}`] && window[`unmount${name}`](`${name}-container`);
+    }
+
+    renderMicroApp= () => {
+        const { name, window, history } = this.props;
+
+        window[`render${name}`] && window[`render${name}`](`${name}-container`, history);
+    };
+
+    render() {
+        return <div id={`${this.props.name}-container`} style={{width: "100%"}} />;
+    }
+}
+
+MicroAppLoader.defaultProps = {
+    document,
+    window,
+};
+
+export default MicroAppLoader;
