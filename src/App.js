3bc7f9432a9312e64ed7078912672641ab68c363
danlaptev <danlaptev@yandex-team.ru>
2022-04-05 16:49:36 +0300

    auto deploying

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
     console.log('process.env.NODE_ENV: ', process.env.NODE_ENV);
     return (
         <div className="App">
-            <h1>Root App</h1>
+            <h1>Root App with Auto Deploying</h1>
             <div className="row">
                 <MicroApp1/>
                 <MicroApp2/>
